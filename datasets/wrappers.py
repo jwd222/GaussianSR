46970a7591fc06981fb0717c53e7c927378a3d67
Jintong Hu <57203330+tljxyys@users.noreply.github.com>
2024-07-12 18:05:52 +0800

    Update wrappers.py

--- datasets/wrappers.py
+++ datasets/wrappers.py
@@ -74,7 +74,8 @@ class SRImplicitPaired(Dataset):
             'inp': crop_lr,
             'coord': hr_coord,
             'cell': cell,
-            'gt': hr_rgb
+            'gt': hr_rgb,
+            'scale': s,
         }
 
 
@@ -88,7 +89,7 @@ def resize_fn(img, size):
 class SRImplicitDownsampled(Dataset):
 
     def __init__(self, dataset, inp_size=None, scale_min=1, scale_max=None,
-                 augment=False, sample_q=None):
+                 augment=False, sample_q=None, batch_per_gpu=4):
         self.dataset = dataset
         self.inp_size = inp_size
         self.scale_min = scale_min
@@ -97,13 +98,23 @@ class SRImplicitDownsampled(Dataset):
         self.scale_max = scale_max
         self.augment = augment
         self.sample_q = sample_q
+        self.last_s = random.uniform(self.scale_min, self.scale_max)
+        self.batch_per_gpu = batch_per_gpu
+        self.call_count = -2
 
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self, idx):
+
+        self.call_count += 1
+        if self.call_count % self.batch_per_gpu == 0:
+            s = random.uniform(self.scale_min, self.scale_max)
+            self.last_s = s
+        else:
+            s = self.last_s
+            
         img = self.dataset[idx]
-        s = random.uniform(self.scale_min, self.scale_max)
 
         if self.inp_size is None:
             h_lr = math.floor(img.shape[-2] / s + 1e-9)
@@ -152,86 +163,8 @@ class SRImplicitDownsampled(Dataset):
             'inp': crop_lr,
             'coord': hr_coord,
             'cell': cell,
-            'gt': hr_rgb
-        }
-
-
-@register('sr-implicit-downsampled-uniform-patch')
-class SRImplicitDownsampledUniformPatchSize(Dataset):
-
-    def __init__(self, dataset, inp_size_min=None, inp_size_max=None, scale_min=1, scale_max=None,
-                 augment=False, sample_q=None):
-        self.dataset = dataset
-        self.inp_size = None
-        self.inp_size_min = inp_size_min
-        if inp_size_max is None:
-            inp_size_max = inp_size_min
-        self.inp_size_max = inp_size_max
-        self.scale_min = scale_min
-        if scale_max is None:
-            scale_max = scale_min
-        self.scale_max = scale_max
-        self.augment = augment
-        self.sample_q = sample_q
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, idx):
-        img = self.dataset[idx]
-        s = random.uniform(self.scale_min, self.scale_max)
-        if self.inp_size_min is None and self.inp_size_max is None:
-            inp_size = None
-        else:
-            inp_size = random.randint(self.inp_size_min, self.inp_size_max)
-        if inp_size is None:
-            h_lr = math.floor(img.shape[-2] / s + 1e-9)
-            w_lr = math.floor(img.shape[-1] / s + 1e-9)
-            img = img[:, :round(h_lr * s), :round(w_lr * s)]  # assume round int
-            img_down = resize_fn(img, (h_lr, w_lr))
-            crop_lr, crop_hr = img_down, img
-        else:
-            w_lr = inp_size
-            w_hr = round(w_lr * s)
-            x0 = random.randint(0, img.shape[-2] - w_hr)
-            y0 = random.randint(0, img.shape[-1] - w_hr)
-            crop_hr = img[:, x0: x0 + w_hr, y0: y0 + w_hr]
-            crop_lr = resize_fn(crop_hr, w_lr)
-
-        if self.augment:
-            hflip = random.random() < 0.5
-            vflip = random.random() < 0.5
-            dflip = random.random() < 0.5
-
-            def augment(x):
-                if hflip:
-                    x = x.flip(-2)
-                if vflip:
-                    x = x.flip(-1)
-                if dflip:
-                    x = x.transpose(-2, -1)
-                return x
-
-            crop_lr = augment(crop_lr)
-            crop_hr = augment(crop_hr)
-
-        hr_coord, hr_rgb = to_pixel_samples(crop_hr.contiguous())
-
-        if self.sample_q is not None:
-            sample_lst = np.random.choice(
-                len(hr_coord), self.sample_q, replace=False)
-            hr_coord = hr_coord[sample_lst]
-            hr_rgb = hr_rgb[sample_lst]
-
-        cell = torch.ones_like(hr_coord)
-        cell[:, 0] *= 2 / crop_hr.shape[-2]
-        cell[:, 1] *= 2 / crop_hr.shape[-1]
-
-        return {
-            'inp': crop_lr,
-            'coord': hr_coord,
-            'cell': cell,
-            'gt': hr_rgb
+            'gt': hr_rgb,
+            'scale': s,
         }
 
 
